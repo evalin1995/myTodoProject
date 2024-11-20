d08a7cfcc076dffa47471fe5c615e0a873745d4c
evalin1995 <nhglje250@gmail.com>
2024-11-20 14:52:57 +0800

    1120

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // 建立一個開發中的路徑
   // process.env.NODE_ENV =>取得node環境中的變數
-  base:process.env.NODE_ENV ==='production' ?'/myYodoProject/':'/',
+  base:process.env.NODE_ENV ==='production' ?'/myTodoProject/':'/',
   plugins: [react()],
 })
